68a47a854038eb435dee354573292f1fa4ece74c
mtc2013 <mtc2013opensource@gmail.com>
2013-07-19 10:12:06 -0400

    Added text All for default category and modified view spec to verify; moved around search field and category per client request; greying out left to do

--- spec/views/organizations/index.html.erb_spec.rb
+++ spec/views/organizations/index.html.erb_spec.rb
@@ -34,7 +34,9 @@ describe "organizations/index.html.erb", :js => true do
     rendered.should have_selector "form input[type='submit']"
     rendered.should have_selector "form input[value='search']"
     rendered.should have_selector "form select[name='category[id]']"
-    rendered.should have_selector "form select[name='category[id]'] option[value='']"
+    rendered.should have_selector "form select[name='category[id]'] option[value='']" do |all_select|
+      expect(all_select).to contain("All")
+    end
     rendered.should have_selector "form select[name='category[id]'] option[value='1']"
     rendered.should have_selector "form select[name='category[id]'] option[value='2']"
   end
