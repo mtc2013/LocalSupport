4ca02869b4341f0980aab11174ab2e326ccf15a0
johnnymo87 <johnnymo87@gmail.com>
2014-02-07 17:21:23 +0000

    reducing it-blocks to bare minimum

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,28 +7,17 @@ describe UsersController do
       controller.stub(:current_user).and_return(admin_user)
 
       @nonadmin_user = double('User', admin?: false, email: 'joetheeskimo@gmail.com')
-      User.stub(:find_by_id).with('4').and_return(@nonadmin_user)
-      @nonadmin_user.stub(:pending_organization_id=).with('5')
-      @nonadmin_user.stub(:save!)
-
-      @org = double('Organization', name: 'Red Cross')
-      Organization.stub(:find).and_return(@org)
+      User.should_receive(:find_by_id).and_return(@nonadmin_user)
     end
     context 'user requesting pending status to be admin of charity' do
-      it 'should update the pending organization id to nested org id' do
-        User.should_receive(:find_by_id).with('4').and_return(@nonadmin_user)
+      it 'redirects to the show page for org and flash success' do
+        org = double('Organization', name: 'Red Cross')
+        Organization.should_receive(:find).and_return(org)
         @nonadmin_user.should_receive(:update_attributes!)
+
         put :update, id: 4, organization_id: 5
-      end
-      it 'should redirect to the show page for nested org' do
-        @nonadmin_user.should_receive(:update_attributes!)
-        put :update, id: 4, organization_id: 5
-        expect(response).to redirect_to(organization_path(5))
-      end
-      it 'should display that a user has requested admin status for nested org' do
-        @nonadmin_user.should_receive(:update_attributes!)
-        put :update, id: 4, organization_id: 5
-        flash[:notice].should have_content "You have requested admin status for #{@org.name}"
+        response.should redirect_to(organization_path(5))
+        flash[:notice].should have_content "You have requested admin status for #{org.name}"
       end
     end
     context 'admin promoting user to charity admin' do
@@ -38,21 +27,11 @@ describe UsersController do
         put :update, {:id => '4'}
         response.should redirect_to '/'
       end
-
-      it "calls a model method to add user's organization and remove user's pending organization" do
-        @nonadmin_user.should_receive(:update_attributes!)
-        @nonadmin_user.should_receive(:promote_to_org_admin)
-        put :update, {:id => '4'}
-      end
-      it 'redirect to index page after update succeeds' do
+      it 'redirects to index page and flash success' do
         @nonadmin_user.should_receive(:update_attributes!)
         put :update, {:id => '4'}
         response.should redirect_to users_path
-      end
-      it 'shows a flash telling which user got approved' do
-        @nonadmin_user.should_receive(:update_attributes!)
-        put :update, {:id => '4'}
-        expect(flash[:notice]).to have_content("You have approved #{@nonadmin_user.email}.")
+        flash[:notice].should have_content("You have approved #{@nonadmin_user.email}.")
       end
     end
   end
