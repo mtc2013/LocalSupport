b082f2cecfa6b41c1ba3c9ab04006eb7a3b81ba9
mtc2013 <mtc2013opensource@gmail.com>
2014-09-07 10:38:00 -0400

    Make tests pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < Devise::SessionsController
 
   def create
     super
-    debugger
     if params[:pending_organization_id] && current_user
       current_user.request_admin_status params[:pending_organization_id]
     end
